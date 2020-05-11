c76a4506c0caeb4ebc3c33cc67f1f50fa0228401
Brandon T. Willard <brandonwillard@gmail.com>
2020-05-11 12:57:04 -0500

    Add some LaTeX setup for amsart; more bullet points

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ This repo contains the source for the tentatively titled "miniKanren for Statist
 
 # Requirements
 
-The source document is in `org-mode` and it is exported to LaTeX using a customized exporter provided by the [`org-btw`](https://github.com/brandonwillard/org-btw) package.
+The source document is in `org-mode` and it is exported to LaTeX using a customized exporter, `ox-latex+`, provided by the [`org-btw`](https://github.com/brandonwillard/org-btw) package (see its use in the `.dir-locals.el` file).  See `org-export-init.el` and the Cask dependencies for an example of a minimal `org-mode` setup that can generate these files.
 
 The generated LaTeX document uses the `amsart` style, which can be installed on Debian variants using
 ```sh
---
+++ src/org/org-export-init.el
@@ -0,0 +1,50 @@
+(add-to-list 'load-path (or (getenv "CASK_DIR") "~/.cask"))
+(require 'cask)
+(cask-initialize "./")
+
+(setq working-dir (f-dirname (f-this-file)))
+(add-to-list 'load-path working-dir)
+
+(setq make-backup-files nil)
+(setq load-prefer-newer t)
+
+(setq backtrace-line-length 0
+      debug-on-error t)
+
+(require 'org)
+(require 'ox)
+(require 'f)
+(require 'org-ref)
+(require 'org-ref+)
+(require 'ox-latex+)
+
+(org-ref+-mode +1)
+
+(setq org-export-allow-bind-keywords t)
+(setq org-export-backends '(latex html))
+
+(add-to-list 'org-babel-load-languages '(emacs-lisp . t))
+(add-to-list 'org-babel-load-languages '(org . t))
+(add-to-list 'org-babel-load-languages '(latex . t))
+(add-to-list 'org-babel-load-languages '(python . t))
+
+(org-babel-do-load-languages 'org-babel-load-languages org-babel-load-languages)
+
+(setq org-latex-listings 'minted
+      org-latex-prefer-user-labels t
+      org-confirm-babel-evaluate nil
+      org-latex-packages-alist '(("" "minted"))
+      org-babel-default-inline-header-args '((:exports . "code") (:eval . "never") (:results . "none")))
+
+(add-to-list 'org-publish-project-alist
+             `("TeX"
+               :base-directory ,working-dir
+               :publishing-directory ,(getenv "TEX_DIR")
+               :figure-dir ,(getenv "FIG_DIR")
+               :base-extension "org"
+               :include (,(f-full (getenv "PUB_FILE")))
+               :publishing-function org-latex+-publish-to-latex))
+
+(setq org-publish-use-timestamps-flag nil)
+
+(pp org-publish-project-alist)
